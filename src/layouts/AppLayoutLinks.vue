c84ed2c0b1aa956f04bd3cb676bacbce2b61965b
Manu <operamike@mail.ru>
2020-11-09 01:17:13 +0100

    Added test route

--- src/layouts/AppLayoutLinks.vue
+++ src/layouts/AppLayoutLinks.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/contacts">Contacts</router-link>
+    <router-link to="/contacts">Contacts</router-link> |
+    <router-link to="/test">Test</router-link>
   </div>
 </template>
 
--- src/router.js
+++ src/router.js
@@ -27,6 +27,11 @@ const routes = [
     meta: {
       layout: 'AppLayoutContacts'
     }
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: () => import('@/views/Home.vue')
   }
 ]
 
